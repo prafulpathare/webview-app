235470d5c84360ef08141adc766384168c40f4df
prafulpathare <patharepraful@gmail.com>
2021-07-05 19:51:52 +0530

    added sidebar nav

---
+++ static/js/def.js
@@ -0,0 +1,10 @@
+$("#sidebar-cancel-btn").click(function (){
+    $("#side-nav-cntr").animate({
+        marginLeft: "-100%"
+    })
+})
+$("#sidebar-btn").click(function (){
+    $("#side-nav-cntr").animate({
+        marginLeft: 0
+    })
+})
